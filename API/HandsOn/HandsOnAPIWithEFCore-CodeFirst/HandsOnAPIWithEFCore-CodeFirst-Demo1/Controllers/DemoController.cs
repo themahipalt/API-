66c5619ab95de459c33696e44cdad35f5f91f714
sandotnet <87011674+sandotnet@users.noreply.github.com>
2022-02-28 17:57:02 +0530

    new_commit

---
+++ API/HandsOn/HandsOnAPIWithEFCore-CodeFirst/HandsOnAPIWithEFCore-CodeFirst-Demo1/Controllers/DemoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HandsOnAPIWithEFCore_CodeFirst_Demo1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetDetails/{id}/{name}")]
+        public IActionResult Get(int id,string name)
+        {
+            return Ok("Method Access");
+        }
+    }
+}
